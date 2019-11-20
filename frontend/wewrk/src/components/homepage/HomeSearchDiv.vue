9ed670ab51798b2116040cdb2c4b6b2b029f05bf
reallynewbie <bruceleung@shaw.ca>
2019-11-20 14:25:46 -0700

    Add Homepage Components

---
+++ frontend/wewrk/src/components/homepage/HomeSearchDiv.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="searchDiv">
+    <h1 id="searchDesc">Search by job keyword, company, or location</h1>
+    <div class="searchbox">
+      <b-input-group id="inputGroup">
+        <b-form-input />
+        <b-input-group-append>
+          <b-button type="submit" id="btn_findwork"><b-img src="./assets/magnifying_glass.png" id="magnifyingGlass"></b-img>Find work</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#magnifyingGlass {
+  width:23.5px;
+  height:23.5px;
+  margin-right:9px;
+}
+.searchDiv {
+  width: 44%;
+}
+#searchDesc {
+  font-family: Rubik;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.19;
+  letter-spacing: normal;
+  text-align: left;
+  color: white;
+}
+
+#inputGroup {
+  height: 100%;
+}
+.form-control {
+  height: 100%;
+}
+.searchbox {
+  width: 635px;
+  height: 72px;
+  border-radius: 6px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  border: solid 1px #dededf;
+  background-color: #ffffff;
+}
+#btn_findwork {
+
+  background-color: #eed350;
+  color: #2f2f2f;
+  font-family: Rubik;
+  font-size: 18px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.22;
+  letter-spacing: normal;
+  text-align: center;
+  color: #2f2f2f;
+  height: 100%;
+  width: 170px;
+  border-style: none;
+}
+</style>
