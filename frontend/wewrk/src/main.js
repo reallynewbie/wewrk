f9ef8393ac6d4c7af18b92ee8689d3cec2f2814f
reallynewbie <bruceleung@shaw.ca>
2019-11-26 14:25:31 -0700

    Add FontAwesome Dependency

--- frontend/wewrk/src/main.js
+++ frontend/wewrk/src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './custom.scss'
+
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
